810c3bc46771c6a03f395a058e975c7c4e1e41f1
Akio Morimoto <akio.morimoto@airits.jp>
2017-04-13 14:31:17 +0900

    refacter

--- todo/server.go
+++ todo/server.go
@@ -1,16 +1,14 @@
-package todo
+package server
 
 import (
   "fmt"
   "net/http"
-  "google.golang.org/appengine"
-  "google.golang.org/appengine/datastore"
 )
 
 func init() {
-  http.HandleFunc("/", index)
+  http.HandleFunc("/todo", handler)
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprint(w, "Hello, TODO")
+func handler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Request: %s - %s", r.Method, r.URL.Path)
 }
