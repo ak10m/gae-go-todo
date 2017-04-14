ca00d6af2b0253677816d05044bee78cfceaa1e2
Akio Morimoto <akio.morimoto@airits.jp>
2017-04-14 19:18:59 +0900

    gofmt

--- server.go
+++ server.go
@@ -1,35 +1,35 @@
 package server
 
 import (
-  "fmt"
-  "net/http"
-  "encoding/json"
+	"encoding/json"
+	"fmt"
+	"net/http"
 )
 
 type Task struct {
-  Title string `json:"title"`
+	Title string `json:"title"`
 }
 
 func init() {
-  http.HandleFunc("/todo", handler)
+	http.HandleFunc("/todo", handler)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 
-  switch r.Method {
-    case "GET":
-      fmt.Fprintln(w, "[]")
-    case "POST":
-      task := Task{ Title: r.FormValue("title") }
-      out, err := json.Marshal(task)
-      if err != nil {
-        w.WriteHeader(422)
-        return
-      }
-      w.WriteHeader(201)
-      fmt.Fprintln(w, string(out))
-    default:
-      w.WriteHeader(400)
-  }
+	switch r.Method {
+	case "GET":
+		fmt.Fprintln(w, "[]")
+	case "POST":
+		task := Task{Title: r.FormValue("title")}
+		out, err := json.Marshal(task)
+		if err != nil {
+			w.WriteHeader(422)
+			return
+		}
+		w.WriteHeader(201)
+		fmt.Fprintln(w, string(out))
+	default:
+		w.WriteHeader(400)
+	}
 }
